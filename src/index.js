84b507c52800a7b9ea19adb082cdd656bf111de6
CanniJr <cannigialaluw@gmail.com>
2020-11-29 21:09:13 -0500

    into react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Testing from './Testing'
 
+const rootElement = document.getElementById('root')
 const jsxElement = <h1>This is a JSX element</h1>
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Cannigia Laluw</p>
+    <small>Nov 29, 2020</small>
+  </header>
+)
+
+
 ReactDOM.render(
-<Testing/>,
-  document.getElementById('root')
+ header, rootElement
 );
 
